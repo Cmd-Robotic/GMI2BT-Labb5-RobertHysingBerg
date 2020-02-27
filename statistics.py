fed5027d9b182701a6f81d6dd1d49b7ab8207305
Robert Hysing Berg <h19robbe@du.se>
2020-02-27 09:56:32 +0100

    Minor changes

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-data = pd.read_excel('./EN0203AD.xlsx')
+data = pd.read_excel('./EnergyAndFuelUsage.xlsx')
 print(data.describe())
+print(data.dtypes)
