24ac1b5faf3909dbacfac940def2c786b4c418a4
Robert Hysing Berg <h19robbe@du.se>
2020-03-06 16:22:35 +0100

    Finalized figure creation, ready for analysis

--- statistics.py
+++ statistics.py
@@ -7,15 +7,44 @@ import matplotlib.pyplot as plt
 title = 'Elproduktion och Bränsleanvändning'
 data = pd.read_excel('./EnergyAndFuelUsage.xlsx')
 data.dropna(inplace=True) # dropping the nan values makes our life easier since it also gets rid of the trailing info text
-print(data)
+# print(data)
 data.rename(columns={'Elproduktion och bränsleanvändning (MWh) efter region, produktionssätt, bränsletyp och år': 'Län', 'Unnamed: 1': 'Produktionssätt', 'Unnamed: 2': 'Bränsletyp'}, inplace=True)
-print(data.dtypes)
+# print(data.dtypes)
 data = data.replace('..', np.nan)
 data.iloc[:, 4:20].astype('float64', copy=False)
-print(data.dtypes)
-print(data.iloc[:,4] + data.iloc[:,5])
-#TODO put this in a while loop with variables so it plots everything
-data.iloc[0:7,3:].transpose().set_axis(data.iloc[0:7,2], axis=1).plot(kind='line', legend=True, title=(data.iloc[0,0] + ', ' + data.iloc[0,1]))
+# print(data.dtypes)
+# print(data.iloc[:,4] + data.iloc[:,5])
+# fig, axes = plt.subplots(nrows=1,ncols=2)
+plottingPosition = 24  #Should be incremented in steps of 8
+startPlotPosition = plottingPosition
+plotType = 'line'
+plotCounter = 0     #where to start plotting from
+plottingStop = 21   #the number of regions to plot - 1 or it's gonna throw an exception
+subplotRow = 0
+subplotColumn = 0
+# Plotting the country first so it doesn't overshadow the rest of the data
+data.iloc[plottingPosition:plottingPosition+1,3:].transpose().set_axis(data.iloc[plottingPosition:plottingPosition+1,0], axis=1).plot(kind=plotType, legend=True, title=(data.iloc[plottingPosition,1] + ', ' + data.iloc[plottingPosition,2]))
+plottingPosition += 40
+plt.xlabel('År')
+plt.ylabel('MWh')
+#Initial plot so we can conatenate two dataframes together
+plotData = data.iloc[plottingPosition:plottingPosition+1,3:].transpose().set_axis(data.iloc[plottingPosition:plottingPosition+1,0], axis=1)
+plottingPosition += 40
+while(plotCounter < plottingStop / 2 - 1):
+    plotData = pd.concat([plotData, data.iloc[plottingPosition:plottingPosition+1,3:].transpose().set_axis(data.iloc[plottingPosition:plottingPosition+1,0], axis=1)], axis=1, sort=False)
+    plottingPosition += 40
+    plotCounter += 1
+# Splitting the graph into two
+plotData.plot(kind=plotType, legend=True, title=(data.iloc[startPlotPosition,1] + ', ' + data.iloc[startPlotPosition,2]))
+plt.xlabel('År')
+plt.ylabel('MWh')
+plotData = data.iloc[plottingPosition:plottingPosition+1,3:].transpose().set_axis(data.iloc[plottingPosition:plottingPosition+1,0], axis=1)
+plottingPosition += 40
+while(plotCounter < plottingStop):
+    plotData = pd.concat([plotData, data.iloc[plottingPosition:plottingPosition+1,3:].transpose().set_axis(data.iloc[plottingPosition:plottingPosition+1,0], axis=1)], axis=1, sort=False)
+    plottingPosition += 40
+    plotCounter += 1
+plotData.plot(kind=plotType, legend=True, title=(data.iloc[startPlotPosition,1] + ', ' + data.iloc[startPlotPosition,2]))
 plt.xlabel('År')
 plt.ylabel('MWh')
 plt.show()
